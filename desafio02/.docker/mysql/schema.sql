07a1e67a75c1aa5bf42e9f4f2d4d36a0a9d6a90f
Vitor A Leal <vitorlealeng@gmail.com>
2024-10-10 09:55:34 -0300

    fc2: Add MYSQL

---
+++ desafio02/.docker/mysql/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE people (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255),
+    PRIMARY KEY (id)
+);
--- desafio02/src/index.js
+++ desafio02/src/index.js
@@ -1,8 +1,26 @@
 const express = require('express')
+var mysql = require('mysql2/promise');
+const { faker } = require('@faker-js/faker');
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('<h1>Full Cycle Rocks!</h1>')
-})
+app.get('/', async (req, res) => {
+  const connection = await mysql.createConnection({
+    host: 'db',
+    database: 'fc2-db',
+    user: 'fc2-user',
+    password: 'password'
+  });
+
+  connection.connect();
+  const insert = 'INSERT INTO `people` (name) VALUES (?)';
+  const values = [faker.person.fullName()];
+  await connection.execute(insert, values);
+  const select = 'SELECT id, name FROM `people`';
+  const [rows, fields] = await connection.query(select);
+  const peopleList = rows.reduce((acc, person) => acc + `<li>${person.id} - ${person.name}</li>`, '');
+  res.send(`<h1>Full Cycle Rocks!!</h1><div><h4></h4><ul>${peopleList}</ul></div>`);
+  connection.end();
+});
 
 app.listen(3000)

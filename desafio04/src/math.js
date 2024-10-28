bae3a3ec658ba38d3e187ef116cab1d8edc7504c
Vitor Leal <vitorlealeng@gmail.com>
2024-10-28 08:21:05 -0300

    feature: add new operations

--- desafio04/src/math.js
+++ desafio04/src/math.js
@@ -2,6 +2,18 @@ class Math {
   sum(a, b) {
     return a + b;
   }
+
+  sub(a, b) {
+    return a - b;
+  }
+
+  times(a, b) {
+    return a * b;
+  }
+
+  div(a, b) {
+    return a / b;
+  }
 }
 
 module.exports = Math;
